5577e60f61ef6254815631abd81956c31ac4a77e
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2020-11-26 08:45:16 -0300

    Update atividade1.c

--- Atividade01/atividade1.c
+++ Atividade01/atividade1.c
@@ -1,30 +1,39 @@
+
+  
 #include <stdio.h>
 #include <stdlib.h>
-
+//4 - P_0 = (2,4), P_end = (10,4)
+//5 - P_0 = (30,5), P_end = (30,15)
 // 6 - P_0 = (10,20), P_end = (15,25)
 
-void bresenham(int x1, int x2, int y1, int y2)
-{
-    int m, dx, dy, incSup, incInf, p, x, y;
-    int valor;
-    dx = x2 - x1;
-    dy = y2 - y1;
-    p = 2 * dy - dx;
-
-    incInf = 2 * dy;
-    incSup = 2 * (dy - dx);
-    x = x1;
-    y = y1;
-
-    //     m = 3 / 8  => 0 < m < 1 ->incrementos unitários em x
-    // dx = 8
-    // dy = 3
-    // 2dy = 6
-
+void bresenham1(int x1, int y1, int x2, int y2){        
+        int slope;
+        int dx, dy, incE, incNE, d, x, y;
+        // Onde inverte a linha x1 > x2       
+        if (x1 > x2 && y1 > y2){
+            bresenham1(x2, y2, x1, y1);
+             return;
+        }        
+        dx = x2 - x1;
+        dy = y2 - y1;
+    
+        if (dy < 0){            
+            slope = -1;
+            dy = -dy;
+        }
+        else{            
+           slope = 1;
+        }
+        // Constante de Bresenham
+        incE = 2 * dy;
+        incNE = 2 * dy - 2 * dx;
+        d = 2 * dy - dx;
+        y = y1;
+        
     printf("dx = %d\n", dx);
     printf("dy = %d\n",dy);
-    printf("2dy = %d\n",incInf);
-    printf("2dy-2dx %d\n",incSup);
+    printf("2dy = %d\n",incE);
+    printf("2dy-2dx %d\n",incNE);
 
     int tam;
     if (x2 - x1 > 0){
@@ -33,30 +42,28 @@ void bresenham(int x1, int x2, int y1, int y2)
         tam = -(x2 - x1);
     }
 
-        int vecX[tam];
+    int vecX[tam];
     int vecY[tam];
     int vecPk[tam];
-
-    int i = 0;
-    while (x < x2)
-    {
-        if (p < 0)
-        {
-            p = p + incInf;
-        }
-        else
-        {
-            p = p + incSup;
-            y++;
-        }
-        x++;
-
-        vecPk[i] = p;
-        vecX[i] = x;
+    
+    int i =0;
+        
+        while(x1 < x2){
+            if (d < 0){
+              d = d + incE;
+            }
+            else{
+              d = d + incNE;
+              y = y + slope;
+            }
+            
+        vecPk[i] = d;
+        vecX[i] = x1;
         vecY[i] = y;
         i++;
-    }
-
+        x1++;
+        }
+        
     printf("X  | ");
     for (int i = 0; i < tam; i++)
     {
@@ -77,7 +84,7 @@ void bresenham(int x1, int x2, int y1, int y2)
         printf("%d ", vecPk[i]);
     }
     printf("\n");
-}
+ }
 
 void imprime(int *vec, int tam)
 {
@@ -89,6 +96,6 @@ void imprime(int *vec, int tam)
 
 int main(void)
 {
-    bresenham(15,10,25,20);
+    bresenham1(30,5,30,15);
     return 0;
 }
